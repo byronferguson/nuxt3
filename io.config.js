b7d98c612fa2718231d0c374a6449916efe502f3
Byron Ferguson <ferguson.bs@gmail.com>
2023-05-03 12:23:52 -0500

    initial boilerplate from https://github.com/lloydtao/nuxt-3-starter

---
+++ io.config.js
@@ -0,0 +1,24 @@
+// socket.io configuration
+export const io = {
+         // we could have multiple sockets that we identify with names
+         // one of these sockets may have set "default" to true
+         sockets: [
+           {
+             default: true, // make this the default socket
+             name: 'main', // give it a name that we can later use to choose this socket in the .vue file
+             url: 'localhost:3700' || process.env.WS_URL, // URL wherever your socket IO server runs
+             vuex: {
+               mutations: [
+                 {
+                   msgToClient: 'socket/ADD_MESSAGE',
+                 },
+               ],
+               actions: [
+                 {
+                   crmTicketCreate: 'crmAlert/displayCrmAlert',
+                 },
+               ],
+             },
+           },
+         ],
+       };
