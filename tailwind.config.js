b0b60da395de04cd1874cd2fd29ef2e9752cedee
Byron Ferguson <ferguson.bs@gmail.com>
2023-05-04 00:46:48 -0500

    chore: :wrench: override tailwind screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,24 @@ module.exports = {
     './nuxt.config.{js,ts}',
   ],
   theme: {
+    // Consider reducing breakpoints to align with expected iPads and iMacs, and rename them to match
+    // https://tailwindcss.com/docs/screens#using-custom-screen-names
+    screens: {
+      'ipad-v': '768px',
+      'ipad-h': '1024px',
+      imac: '1280px',
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'dark-primary': '#1F145D',
+        'medium-primary': '#2157D6',
+        'light-primary': '#99E3FA',
+        secondary: '#F8564B',
+        blush: '#FFD0D3',
+        gold: '#FFCF34',
       },
     },
   },
