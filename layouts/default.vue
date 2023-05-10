bcacfead7ffc8795513c2be793c69dd180ba9485
Byron Ferguson <ferguson.bs@gmail.com>
2023-05-10 09:40:02 -0500

    feat: :sparkles: add version to footer, part 2 :grin:

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,14 +7,30 @@ useHead({
     class: 'h-full',
   },
 });
+
+const version = useRuntimeConfig().public.VERSION;
+const year = new Date().getFullYear();
 </script>
 
 <template>
   <div class="min-h-screen">
     <NavBar />
 
-    <main class="py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-      <slot />
-    </main>
+    <div class="flex flex-col py-6 mx-auto max-w-7xl sm:px-6 lg:px-8 min-h-screen-minus-nav">
+      <main class="flex-grow">
+        <slot />
+      </main>
+      <footer
+        class="container px-12 mx-auto mt-6 mb-0 text-sm font-semibold text-right text-gray-600"
+      >
+        v{{ version }}-N &copy;{{ year }} Big Blue Swim School LLC
+      </footer>
+    </div>
   </div>
 </template>
+
+<style lang="postcss" scoped>
+.min-h-screen-minus-nav {
+  min-height: calc(100vh - 64px);
+}
+</style>
