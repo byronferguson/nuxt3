ef7151599d7faf6ec8e49b6aeccad3f91d9f0f33
Byron Ferguson <ferguson.bs@gmail.com>
2023-05-10 08:52:07 -0500

    feat: :sparkles: add basic Button with SB story

--- components/global/Button.vue
+++ components/global/Button.vue
@@ -1,7 +1,60 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+interface ButtonProps {
+  /**
+   * Button "type" attribute
+   */
+  type?: 'button' | 'submit' | 'reset';
+  /**
+   * Background color of the button
+   */
+  color?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark';
+}
+
+const props = withDefaults(defineProps<ButtonProps>(), {
+  type: 'button',
+});
+</script>
 
 <template>
-  <button type="button" class="px-3 py-1 text-white rounded bg-dark-primary">
+  <button :type="type" class="btn" :class="color">
     <slot />
   </button>
 </template>
+
+<style lang="postcss" scoped>
+.btn {
+  @apply px-3 py-1 rounded;
+}
+
+.primary {
+  @apply bg-dark-primary text-white;
+}
+
+.secondary {
+  @apply bg-medium-primary text-white;
+}
+
+.success {
+  @apply bg-green-500 text-white;
+}
+
+.danger {
+  @apply bg-secondary text-white;
+}
+
+.warning {
+  @apply bg-gold text-black;
+}
+
+.info {
+  @apply bg-light-primary text-black;
+}
+
+.light {
+  @apply bg-gray-200 text-black;
+}
+
+.dark {
+  @apply bg-gray-800 text-white;
+}
+</style>
--- stories/Button.stories.ts
+++ stories/Button.stories.ts
@@ -15,8 +15,20 @@ const meta = {
       defaultValue: 'Big Blue',
       type: { name: 'string', required: true },
     },
+    type: {
+      control: {
+        type: 'select',
+        options: ['button', 'submit', 'reset'],
+      },
+    },
+    color: {
+      control: {
+        type: 'select',
+        options: ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'],
+      },
+    },
   },
-  args: { default: 'Big Blue' }, // default value
+  args: { default: 'Base' }, // default value
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -26,8 +38,39 @@ type Story = StoryObj<typeof meta>;
  * See https://storybook.js.org/docs/vue/api/csf
  * to learn how to use render functions.
  */
-export const Primary: Story = {};
+export const Base: Story = {};
+
+export const Primary: Story = {
+  args: {
+    default: 'Primary',
+    color: 'primary',
+  },
+};
 
 export const Secondary: Story = {
-  args: { default: 'Secondary' },
+  args: { default: 'Secondary', color: 'secondary' },
+};
+
+export const Success: Story = {
+  args: { default: 'Success', color: 'success' },
+};
+
+export const Danger: Story = {
+  args: { default: 'Danger', color: 'danger' },
+};
+
+export const Warning: Story = {
+  args: { default: 'Warning', color: 'warning' },
+};
+
+export const Info: Story = {
+  args: { default: 'Info', color: 'info' },
+};
+
+export const Light: Story = {
+  args: { default: 'Light', color: 'light' },
+};
+
+export const Dark: Story = {
+  args: { default: 'Dark', color: 'dark' },
 };
