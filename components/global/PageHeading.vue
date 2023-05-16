3e72e399659170e64544d2b19fea7db31f2cab85
Byron Ferguson <ferguson.bs@gmail.com>
2023-05-16 14:33:50 -0500

    style: :lipstick: set PageHeading to uppercase

--- components/global/PageHeading.vue
+++ components/global/PageHeading.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="bg-white shadow-sm">
     <div class="px-4 py-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-      <h1 class="text-lg font-semibold leading-6 text-gray-900">
+      <h1 class="text-lg font-semibold leading-6 text-gray-900 uppercase">
         <slot />
       </h1>
     </div>
