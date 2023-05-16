d687183c58d7340c22fa3022cf910663f908147a
Byron Ferguson <ferguson.bs@gmail.com>
2023-05-16 14:34:18 -0500

    feat: :sparkles: add Request and Receipt forms

--- pages/tools/receipt-submission/index.vue
+++ pages/tools/receipt-submission/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <h1 class="text-2xl">Tools - Receipts</h1>
+  <div class="px-8 py-5 mt-4 bg-white rounded-lg">
+    <IFRAME
+      width="100%"
+      height="1200"
+      frameborder="0"
+      src="https://www.smartsheet.com/b/publish?EQBCT=fe3326e1d0234da3ba48b47aabbf8717"
+    ></IFRAME>
   </div>
 </template>
--- pages/tools/request-to-order/index.vue
+++ pages/tools/request-to-order/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <h1 class="text-2xl">Tools - Request to Order</h1>
+  <div class="px-8 py-5 mt-4 bg-white rounded-lg">
+    <iframe
+      width="100%"
+      height="1200"
+      frameborder="0"
+      src="https://app.smartsheet.com/b/form/793827e990ef4124a55592b08b2fc713"
+    ></iframe>
   </div>
 </template>
